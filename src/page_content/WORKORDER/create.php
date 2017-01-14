b56cf449f854232dd83484ffd01363d87c95cbf4
Raymond Brady <thewizster@gmail.com>
2017-01-14 16:15:14 -0600

    move to workorder page routing

--- src/page_content/WORKORDER/create.php
+++ src/page_content/WORKORDER/create.php
@@ -1,4 +1,34 @@
-<?php
-        require_once('./resources/library/dot.php');
-		 require_once('./includes/pageworkordersnew.php');
-?>
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Create New Workorder
+                </h1>
+                <ol class="breadcrumb">
+                    <li class="active">
+                        <i class="fa fa-file"></i> Available Workorder Forms
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <div class="row">
+            <div class="col-lg-12">
+                <?php
+                        require_once('./resources/library/dot.php');
+                        $dot = new Dot(); // class used for rendering user form related content
+                        $dot->renderUserFormsList("createworkorder.php");
+                ?>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
