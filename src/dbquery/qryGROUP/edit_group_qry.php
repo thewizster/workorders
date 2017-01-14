88e1ac9184f81c70df82276cfc847e7afa925ad0
Raymond Brady <thewizster@gmail.com>
2017-01-14 16:12:01 -0600

    add todo undefined var

--- src/dbquery/qryGROUP/edit_group_qry.php
+++ src/dbquery/qryGROUP/edit_group_qry.php
@@ -5,7 +5,9 @@ $element_function = "EDITED";
 require_once "./resources/library/groups.php";
 $groupDbAdapter = new groupDataAdapter($dsn, $user_name, $pass_word);
 
+// TODO: Resolve undefined variable: What is $general_seed used for? Is currently undefined here
 $GRP_id_active = pg_encrypt($_POST['group_id'].$general_seed,$pg_encrypt_key,"decode"); //$_POST['group_id'];
+
 $GRP_name = $_POST['group_name'];
 //echo "--------------------------------------------------------------------------------------------   "  . $transfer_GRP_id.  "   ------ ".$GRP_id_active;
 $QUERY_PROCESS = $groupDbAdapter->Update($GRP_id_active,$GRP_name);
